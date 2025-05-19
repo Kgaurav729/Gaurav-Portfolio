32a0a7cf9aa82af41f556445dcef4d24bec4cea8
Gaurav <gauravkulmethe007@gmail.com>
2025-05-19 20:33:01 +0530

    changed the ui of project

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -81,7 +81,7 @@ const Projects = () => {
                 ))}
               </div>
               <button className="inline-block px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
-                View Project
+                More Info...
               </button>
             </div>
           </motion.div>
